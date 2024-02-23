971807fafc145d935e1714380c82690f402e14ab
Code2032 <160088042+Code2032@users.noreply.github.com>
2024-02-22 22:35:50 -0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+class Human:
+    eye:str
+    height:float
+    weight:float
+
+    def eating():
+        print("I eat a lot of brains")
+
+
+    def walking():
+        print('i walk 1 million miles per nanosecond')
+
+
+
+john = Human()
+
+
+john.eye ='blue'
+john.eating
+
+
+
+
+
